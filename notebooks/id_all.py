90603404dc928fc85e193b631cf2225bfef97336
Leo <leooooo@bu.edu>
2016-06-27 15:29:07 -0400

    added goodokaybad signals

--- notebooks/id_all.py
+++ notebooks/id_all.py
@@ -9,7 +9,13 @@ class Recognizer:
 			filename = path_to_files+'/{}.png'.format(i)
 			self.no_imgs[i] = self.clean_img(io.imread(filename)[:,:,:3])
 		self.messup_img =  self.clean_img(io.imread(path_to_files+'/messup.png')[35:67,101:153,:3])
-
+		self.gob_names = ['good','okay','bad']
+		gob_thresh = [100,90,95]
+		self.gobs = {}
+		for i in range(3):
+			img = self.clean_img(io.imread('../masks/{}.png'.format(self.gob_names[i])))
+			self.gobs[self.gob_names[i]] = Gob_Signal(img,gob_thresh[i])
+		
 		self.no_thresh = 60
 		self.messup_thresh = 60
 
@@ -20,6 +26,7 @@ class Recognizer:
 		self.messed_up = False
 
 	def clean_img(self,img_to_clean):
+		img_to_clean = img_to_clean[:,:,:3]
 		rgb = [0,255,0]
 		mask = np.where(np.all(img_to_clean == rgb,axis=-1))
 		img_to_clean[mask] = [-1,-1,-1]
@@ -58,7 +65,7 @@ class Recognizer:
 		return False
 
 	def messup_tester(self,img_to_test, thresh=-1,debug=False):
-		if thresh < 0: thresh = self.no_thresh
+		if thresh < 0: thresh = self.messup_thresh
 		res = np.mean(cv2.absdiff(img_to_test,self.messup_img))
 		if debug: return res
 		return res < thresh
@@ -68,4 +75,29 @@ class Recognizer:
 		delta_score = score - self.last_score # remember score is strictly increasing))
 		self.last_score = score
 		messup = self.messup_test(frame[35:67,101:153,:])
-		return {'score':score,'delta_score':delta_score,'messup':messup}
+		tor =  {'score':score,'delta_score':delta_score,'messup':messup}
+		for n in self.gob_names:
+			tor[n] = self.gob[n].test(frame[50:75,15:45,:3])
+		return tor
+
+class Gob_Signal:
+	def __init__(self,img,thresh):
+		self.img = img
+		self.thresh = thresh
+		self.last_on = False
+
+	def test(self,img_to_test):
+		# tests if gob JUST got detected, flips off when no longer detected
+		test_res = np.mean(cv2.absdiff(img_to_test,self.img))
+		res = test_res - thresh
+		if not self.last_on:
+			if res < 0:
+				self.last_on = True
+				return True
+		else:
+			if res > 0:
+				self.last_on = False
+		return False
+
+if __name__ == '__main__':
+	test_check = Recognizer('../masks')
